9b407b2d5da1542dc5f6dd9fe0c7b083feecc6ae
ChiragHooda <138375795+ChiragHooda@users.noreply.github.com>
2023-08-23 22:17:49 +0530

    23 Aug 2023

--- PQBacktracking.java
+++ PQBacktracking.java
@@ -1,30 +1,27 @@
 public class PQBacktracking {
-    public static void ratInMaze(int arr[][],int i,int j) {
+    public static void ratInMaze(int arr[][],int i,int j,int sol[][]) {
         //base coditions
+        sol[i][j]=arr[i][j];
         if(i==arr.length-1&&j==arr.length-1){
             System.out.println("found");
+            printarr(sol);
             return;
         }
-        //up U
-//        if (isSafe(arr, i - 1, j)) {
-//            ratInMaze(arr, i - 1, j);
-//        }else{
-//            return false;
-//        }
         //down D
         if (isSafe(arr, i + 1, j)) {
-            ratInMaze(arr, i + 1, j);
+            ratInMaze(arr, i + 1, j,sol);
         }
-        //left L
-//        if (isSafe(arr, i, j - 1)) {
-//            ratInMaze(arr, i, j - 1);
-//        }else{
-//            return false;
-//        }
         //Right R
         if(isSafe(arr,i,j + 1)){
-            ratInMaze(arr,i,j+1);
+            ratInMaze(arr,i,j+1,sol);
         }
+        if(isSafe(arr,i,j + 1) || isSafe(arr, i + 1, j)){
+            sol[i][j]=1;
+        }
+        if(!isSafe(arr,i,j + 1)&&!isSafe(arr, i + 1, j)){
+            sol[i][j]=0;
+        }
+
     }
 
     public static boolean isSafe(int arr[][],int i,int j){
@@ -33,6 +30,15 @@ public class PQBacktracking {
         }
         return false;
     }
+
+    public static void printarr(int arr[][]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
     static int count=0;
 
     public static void main(String[] args) {
@@ -40,6 +46,7 @@ public class PQBacktracking {
                 { 1, 1, 0, 1 },
                 { 0, 1, 0, 0 },
                 { 1, 1, 1, 1 } };
-        ratInMaze(maze,0,0);
+        int sol[][]=new int[maze.length][maze.length];
+        ratInMaze(maze,0,0,sol);
     }
 }
